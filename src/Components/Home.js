6a2c7cb8e793cd1a9f6fa3f447ff2569d2fd9a30
Aniket Tote <anikettote2001@gmail.com>
2023-03-16 14:36:32 +0530

    added router

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,17 +1,18 @@
 import React from "react";
 import Content from "./Content";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import FirstContetnt from "./FirstContetnt";
 
 export default function Home() {
-  
   return (
-   
-  <>
-  
-
-  <FirstContetnt/>
-    
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<FirstContetnt />} />
+          <Route path="/content" element={<Content />} />
+        </Routes>
+      </Router>
     </>
   );
 }
--- src/Components/HomeSection.js
+++ src/Components/HomeSection.js
@@ -1,16 +1,12 @@
-import React from 'react'
- import VerticalNav from './VerticalNav';
-import Home from './Home';
+import React from "react";
+import VerticalNav from "./VerticalNav";
+import Home from "./Home";
 
-
-export default function
-() {
+export default function () {
   return (
-    
     <div className="flex h-calc-16 w-screen bg-white xs:flex-col-reverse xs:relative xs:p-1">
-<VerticalNav/>
-<Home/>
+      <VerticalNav />
+      <Home />
     </div>
-   
-  )
+  );
 }
